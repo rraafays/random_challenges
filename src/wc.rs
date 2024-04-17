8c5eb60a2bd4f6e910f350023134c77bb387464f
raf <rraf@tuta.io>
2024-04-17 16:11:29 +0100

    update: line count function

--- src/wc.rs
+++ src/wc.rs
@@ -1,4 +1,9 @@
-use std::{fs::File, io::Error, path::Path, process::exit};
+use std::{
+    fs::File,
+    io::{BufRead, BufReader, Error},
+    path::Path,
+    process::exit,
+};
 
 use clap::Parser;
 
@@ -10,17 +15,29 @@ struct Arguments {
 }
 
 fn main() {
-    let arguments = Arguments::parse();
+    let arguments: Arguments = Arguments::parse();
 
     let file: Result<File, Error> = File::open(Path::new(&arguments.path));
-    let _data: File = match file {
-        Ok(file) => {
-            print!("found");
-            file
+    match file {
+        Ok(contents) => {
+            let count: usize = get_lines(&contents).len();
+            println!("lines: {}", count)
         }
         Err(_) => {
             eprintln!("file not found!");
             exit(1);
         }
     };
+}
+
+fn get_lines(file: &File) -> Vec<String> {
+    let mut lines: Vec<String> = Vec::new();
+    let data: BufReader<&File> = BufReader::new(file);
+    for line in data.lines() {
+        match line {
+            Ok(line) => lines.push(line),
+            Err(_) => todo!(),
+        }
+    }
+    lines
 }
