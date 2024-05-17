46eb798c48d98fc443ece95a5d6e3c4d9fb9a230
raf <rraf@tuta.io>
2024-05-17 18:15:00 +0100

    chore: switched to unstable

--- shell.nix
+++ shell.nix
@@ -1,11 +1,14 @@
 { pkgs ? import <nixpkgs> { } }:
+let
+  unstable = import (fetchTarball "https://github.com/NixOS/nixpkgs/archive/nixos-unstable.tar.gz") { };
+in
 pkgs.mkShell {
   nativeBuildInputs = with pkgs.buildPackages; [
     cargo
   ];
   buildInputs = with pkgs; [
-    rustc
-    rust-analyzer
-    rustfmt
+    unstable.rustc
+    unstable.rust-analyzer
+    unstable.rustfmt
   ] ++ lib.optional stdenv.isDarwin libiconv;
 }
